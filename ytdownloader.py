2b87ce60325c4313ebda96d8b4c0c5a26463f198
Atharva <atharvahudlikar@gmail.com>
2020-05-05 16:02:13 +0530

    Final Working code added

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# n = 0
-# urls = []
-# file = open('urls.txt', 'r')
-# lines = file.readlines()
-# for line in lines:
-#     n += 1
-#     urls.append(line)
-
-n = 1
-urls = ['https://www.youtube.com/watch?v=Ftexl9o4QX8&t=12s']
+n = 0
+urls = []
+file = open('urls.txt', 'r')
+lines = file.readlines()
+for line in lines:
+    n += 1
+    urls.append(line)
 
 browser = webdriver.Chrome(executable_path=r'C:\Users\Atharva\chromedriver.exe')
 for i in range(0, n):
     browser.get('https://www.y2mate.com/en19')
-    input_url = browser.find_element_by_class_name('form-control input-lg')
+    input_url = browser.find_element_by_id('txt-url')
     input_url.send_keys(urls[i])
     start_button = browser.find_element_by_id('btn-submit')
     start_button.click()
+    time.sleep(5)
